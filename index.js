8760a4191a50a90ac8aab413b393171fbd6c9f1c
Chaxiraxi <chaxiraxi@theagilemonkeys.com>
2021-03-19 16:52:21 +0000

    Add input validation

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      binaryNumber: '',
+      errorMessage: ''
     },
     methods: {
       convertBinaryToDecimal: function () {
-        this.message = this.message.split('').reverse().join('')
+        if (isValidInput(this.binaryNumber)) {
+          this.binaryNumber = this.binaryNumber.split('').reverse().join('');
+          this.errorMessage = '';
+        } else {
+          this.binaryNumber = '';
+          this.errorMessage = 'You should introduce a Binary Number';
+        }
       }
     }
-})
+});
+
+
+function isValidInput (input) {
+  const binaryRegex = /^[01]+$/;
+  if (input.length != 8) {
+    return false;
+  }
+
+  if (!input.match(binaryRegex)) {
+    return false;
+  }
+
+  return true;
+}
